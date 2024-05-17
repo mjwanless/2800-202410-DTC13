de9908e9c9152f46ef368801e6e87997c657efda
Caroline Su <caroline77899@gmail.com>
2024-05-16 18:13:04 -0700

    chore: update log-out function

--- index.js
+++ index.js
@@ -498,7 +498,7 @@ app.get("/my_preference", (req, res) => {
 app.post("/logout", (req, res) => {
   res.clearCookie("connect.sid", { path: "/" });
   req.session.destroy();
-  // res.render("logout");
+  res.redirect("/");
 });
 
 // 404 Page (Keep down here so that you don't muck up other routes)
