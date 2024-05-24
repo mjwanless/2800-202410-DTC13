d5ac120904ecd4781d685b8a763323115234c06b
Caroline Su <caroline77899@gmail.com>
2024-05-23 23:38:26 -0700

    feat: add evenlistener to increase and minus buttons

---
+++ public/script/controlQuantities.js
@@ -0,0 +1,41 @@
+function decreaseQuantity(product_id, price) {
+    decreaseBtn = document.getElementById("text_" + product_id)
+    if (decreaseBtn.innerText == 1) {
+        return
+    }
+    decreaseBtn.innerText--
+
+    let priceElem = document.getElementById("price_" + product_id);
+    let priceValue = parseFloat(priceElem.innerText.substring(1));
+    priceElem.innerText = "$" + (priceValue - parseFloat(price)).toFixed(2);
+    
+
+    fetch('/quantity/decrease/' + product_id, {
+        method: 'POST',
+    }).then(response => {
+        if (response.ok) {
+            console.log('Quantity reduced')
+        }
+    }
+    )
+
+}
+function increaseQuantity(product_id, price) {
+    increaseBtn = document.getElementById("text_" + product_id)
+    if (increaseBtn.innerText == 1) {
+        return
+    }
+    increaseBtn.innerText++
+
+    let priceElem = document.getElementById("price_" + product_id);
+    let priceValue = parseFloat(priceElem.innerText.substring(1));
+    priceElem.innerText = "$" + (priceValue + parseFloat(price)).toFixed(2);
+    fetch('/quantity/increase/'+product_id, {
+        method: 'POST',
+    }).then(response => {
+        if (response.ok) {
+            console.log('Quantity added')
+        }
+    }
+)
+}
