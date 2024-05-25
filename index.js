3d5e4cd41a1c2c1ab71419e0c2e89922cea1caea
Xinli Wang <shirley.wang.xinli@gmail.com>
2024-05-24 21:17:21 -0700

    chore: refactor sendOrderConfirmation to sendOrderConfirmationEmail

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const feedbacks = require("./createFeedback");
 const User = require("./userSchema");
 const orders = require("./orderSchema");
 const calculator = require("./caloriesCalculator");
-const sendConfirmationEmail = require("./sendOrderConfirmation");
+const sendConfirmationEmail = require("./sendOrderConfirmationEmail");
 const OAuth2 = google.auth.OAuth2; //google auth library to send email without user interaction and consent
 const OAuth2Client = new OAuth2(config.clientId, config.clientSecret); //google auth client
 OAuth2Client.setCredentials({ refresh_token: config.refreshToken });
