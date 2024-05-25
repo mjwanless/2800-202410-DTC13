bc7f6108e11e83c77cc77db39ff33579a89645f7
Xinli Wang <shirley.wang.xinli@gmail.com>
2024-05-24 23:57:47 -0700

    chore: remove resetPassword function and reset_password post router from index

--- index.js
+++ index.js
@@ -196,49 +196,6 @@ const loginValidation = async (req, res, next) => {
   }
 };
 
-// Middleware to reset a password
-const resetPassword = async (req, res, next) => {
-  const schema = joi.object({
-    email: joi.string().max(200).required(),
-  });
-  const validationResult = schema.validate({ email: req.body.email });
-  if (validationResult.error) {
-    res.send("login validation result error", {
-      error: validationResult.error,
-    });
-    return;
-  }
-  try {
-    user = await User.findOne({ email: req.body.email });
-    if (user) {
-      const outputAnswer = user.security_answer;
-      const inputAnswer = req.body.security_answer;
-
-      if (!(await bcrypt.compare(inputAnswer, outputAnswer))) {
-        return res.render("reset_password", { wrongAnswer: true });
-      }
-
-      var hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
-
-      try {
-        await User.findOneAndUpdate(
-          { email: req.body.email },
-          { $set: { password: hashedPassword } }
-        );
-
-        next();
-      } catch (err) {
-        console.error("Failed to update password:", err);
-        res.status(500).send("Failed to update password.");
-      }
-    } else {
-      return res.render("reset_password", { noUser: true });
-    }
-  } catch (err) {
-    console.log("fail to login", err);
-  }
-};
-
 // GET request for the root URL/"Homepage"
 app.get("/", async (req, res) => {
   let reviews = await feedbacks.find({}).sort({ time: -1 }).limit(3);
@@ -360,56 +317,6 @@ app.get("/getSecurityQuestion/:email", async (req, res) => {
     console.error("Failed to retrieve user:", err);
     res.json(null);
   }
-});
-
-// After successful password reset
-app.post("/reset_password", resetPassword, async (req, res) => {
-  const accessToken = getAccessToken();
-  // const accessToken = await OAuth2Client.getAccessToken();
-
-  const user = await User.findOne({ email: req.body.email });
-  const recipient = user.email;
-  const userName = user.username;
-
-  function resetPasswordEmail(recipient) {
-    const transporter = nodeMailer.createTransport({
-      service: "gmail",
-      auth: {
-        type: "OAuth2",
-        user: config.user,
-        clientId: config.clientId,
-        clientSecret: config.clientSecret,
-        refreshToken: config.refreshToken,
-        accessToken: accessToken,
-      },
-    });
-
-    const mailOptions = {
-      from: `Fresh Plate <${config.user}>`,
-      to: recipient,
-      subject: "Password Has Been Reset Successfully",
-      html: resetPasswordInfo(),
-    };
-
-    transporter.sendMail(mailOptions, (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("Email sent: " + result);
-      }
-      transporter.close();
-    });
-  }
-
-  function resetPasswordInfo() {
-    return `
-    <h1>Hello ${userName} ! </h1>
-    <p>Your password has been reset successfully.</p>
-    `;
-  }
-  resetPasswordEmail(recipient);
-
-  res.redirect("/login");
 });
 
 app.use(isAuthenticated);
